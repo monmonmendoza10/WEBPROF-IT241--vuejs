141083c937afbb1fd3b05dcdc571aea8ea7fbce9
monmonmendoza10 <vvmendoza@student.apc.edu.ph>
2026-01-16 09:45:42 +0800

    .

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -2,8 +2,8 @@
 
   <h1>Food</h1>
   <food-item/>
-  <food-item/>
-  <food-item/>
+  <food-item2/>
+ 
 </template>
 
 <script></script>
---
+++ firstsfc/src/components/FoodItem2.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="apple">>
+
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Mango',
+      message: 'I like Mango'
+    }
+  }
+};
+</script>
+
+<style>
+  .mango {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: orange;
+  }
+</style>
